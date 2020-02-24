652f829235c982f973e7354e7e46fcdb8e121ead
Michael Romay <amerror@gmail.com>
2020-02-24 09:47:36 -0800

    added url change

--- data/guitars.js
+++ data/guitars.js
@@ -2,7 +2,7 @@ module.exports = [
     {
         model: 'Les Paul Custom',
         make: 'Gibson',
-        url: '/1988-les-paul-custom-kahler-fronclose.jpg',
+        url: 'https://rocky-cove-46033.herokuapp.com/1988-les-paul-custom-kahler-fronclose.jpg',
         year: 1988,
         is_left_handed: false
     },
@@ -16,42 +16,42 @@ module.exports = [
     {
         model: 'Stratocaster',
         make: 'Fender',
-        url: '/1987-lefthanded-fender-stratocaster.jpg',
+        url: 'https://rocky-cove-46033.herokuapp.com/1987-lefthanded-fender-stratocaster.jpg',
         year: 1988,
         is_left_handed: true
     },
     {
         model: 'GS',
         make: 'Gibson',
-        url: '/1969-gibson-sg-standard-cherry-red.jpg',
+        url: 'https://rocky-cove-46033.herokuapp.com/1969-gibson-sg-standard-cherry-red.jpg',
         year: 1969,
         is_left_handed: false
     },
     {
         model: '914',
         make: 'Taylor',
-        url: '/1978-taylor-914.jpg',
+        url: 'https://rocky-cove-46033.herokuapp.com/1978-taylor-914.jpg',
         year: 1978,
         is_left_handed: false
     },
     {
         model: 'ESP',
         make: 'M-II',
-        url: '/1990-esp-m-ii.jpg',
+        url: 'https://rocky-cove-46033.herokuapp.com/1990-esp-m-ii.jpg',
         year: 1990,
         is_left_handed: false
     },
     {
         model: 'ES-335',
         make: 'Gibson',
-        url: '/1965-Gibson-ES-335-Mono-Sparkling-Burgundy.jpg',
+        url: 'https://rocky-cove-46033.herokuapp.com/1965-Gibson-ES-335-Mono-Sparkling-Burgundy.jpg',
         year: 1965,
         is_left_handed: false
     },
     {
         model: 'Californian',
         make: 'Domino',
-        url: '/1960-Domino-Californian-Electric-Guitar.jpg',
+        url: 'https://rocky-cove-46033.herokuapp.com/1960-Domino-Californian-Electric-Guitar.jpg',
         year: 1960,
         is_left_handed: false
     }
